ffcc0af22495d58672e1329166cd2cd010efb798
unknown <Davidokih>
2022-06-20 15:18:55 +0100

    first-commit

---
+++ utils/sendMail.js
@@ -0,0 +1,5 @@
+const nodeMailer = require('nodemailer');
+const { google } = require('googleapis');
+
+const CLIENT_ID = '541944773634-0krst0fkbsdpfbdfuj0cnjnrcf2jr4q7.apps.googleusercontent.com';
+// const CLIENT_SECRET = ""
