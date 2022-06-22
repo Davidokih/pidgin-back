0fe9f9acbf70f3753398dc0dd6c39d0dc582383b
unknown <Davidokih>
2022-06-22 15:35:16 +0100

    update

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use("/pidgin/user", require("./router/userRouter"));
 app.use("/pidgin/post", require("./router/postRouter"));
 app.use("/pidgin/like", require("./router/likeRouter"));
 app.use("/pidgin/definition", require("./router/definitionRouter"));
-// app.use("/pidgin/bio", require("./router/bioRouter"));
+app.use("/pidgin/bio", require("./router/bioRouter"));
 
 app.listen(port, () => {
 	console.log("server is now up and Running");
